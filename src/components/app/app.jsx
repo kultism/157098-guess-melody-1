8390a8a346379f3644eba7f66447039f02cc3707
Igor Kucherenko <hotncold1989@gmail.com>
2019-05-04 10:49:23 +0300

    add props and proptypes for Welcome component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Welcome from '../welcome';
 
-const App = () => <Welcome/>;
+const App = () => <Welcome sessionTime={7} errorsAllowed={4}/>;
 
 export default App;
