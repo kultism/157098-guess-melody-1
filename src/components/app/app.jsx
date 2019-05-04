a575c6f5339134add1c3802e6df18cb262f90d47
Igor Kucherenko <hotncold1989@gmail.com>
2019-05-04 14:58:28 +0300

    add simple e2e tests for Welcome component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import Welcome from '../welcome';
 
-const App = () => <Welcome sessionTime={7} errorsAllowed={4}/>;
+const handleStartButtonClick = () => null;
+
+const App = () => (
+  <Welcome
+    sessionTime={7}
+    errorsAllowed={4}
+    onButtonClick={handleStartButtonClick}/>
+);
 
 export default App;
---
+++ src/components/welcome/welcome.e2e.test.js
@@ -0,0 +1,24 @@
+import Enzyme, {shallow} from 'enzyme';
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import Welcome from './welcome';
+
+Enzyme.configure({adapter: new Adapter()});
+
+describe(`<Welcome />`, () => {
+  const handleClick = jest.fn();
+
+  it(`simulates click on start button`, () => {
+    const wrapper = shallow(<Welcome
+      errorsAllowed={0}
+      sessionTime={0}
+      onStartButtonClick={handleClick}/>
+    );
+
+    const startButton = wrapper.find(`button`);
+
+    startButton.simulate(`click`);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+});
