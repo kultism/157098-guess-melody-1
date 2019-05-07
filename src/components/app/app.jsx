6459f89deed73e440afcd5ee6f741c9ab17ed279
Igor Kucherenko <hotncold1989@gmail.com>
2019-05-07 21:16:36 +0300

    move props to entry point

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Welcome from '../welcome';
 
 const handleStartButtonClick = () => null;
 
-const App = () => (
-  <Welcome
-    sessionTime={7}
-    errorsAllowed={4}
-    onButtonClick={handleStartButtonClick}/>
-);
+const App = ({questions, gameTime, errorsCount}) => {
+  return (
+    <Welcome
+      gameTime={gameTime}
+      errorsCount={errorsCount}
+      onButtonClick={handleStartButtonClick}/>
+  );
+};
+
+App.propTypes = {
+  questions: PropTypes.array.isRequired,
+  gameTime: PropTypes.number.isRequired,
+  errorsCount: PropTypes.number.isRequired
+};
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import questions from './mocks/questions';
+import gameSettings from './mocks/game-settings';
 
 const init = () => {
-  ReactDOM.render(<App/>, document.querySelector(`.main`));
+  const {gameTime, errorsCount} = gameSettings;
+  ReactDOM.render(<App
+    questions={questions}
+    gameTime={gameTime}
+    errorsCount={errorsCount}/>, document.querySelector(`.main`));
 };
 
 init();
