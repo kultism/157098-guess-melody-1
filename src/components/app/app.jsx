83f44c7338e4778c707c17c7eecf7e589ded3afe
Igor Kucherenko <hotncold1989@gmail.com>
2019-05-19 16:42:00 +0300

    update App component with newly created Header

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -5,39 +5,13 @@ import {ActionCreator} from '../../redux/actions';
 import Welcome from '../welcome';
 import ArtistQuestionScreen from '../artist-question-screen';
 import GenreQuestionScreen from '../genre-question-screen';
+import Header from '../header';
 
 class App extends PureComponent {
   render() {
     return (
       <section className="game">
-        <header className="game__header">
-          <a className="game__back" href="#">
-            <span className="visually-hidden">Сыграть ещё раз</span>
-            <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию"/>
-          </a>
-
-          <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-            <circle
-              className="timer__line" cx="390" cy="390" r="370"
-              style={{
-                filter: `url(#blur)`,
-                transform: `rotate(-90deg) scaleY(-1)`,
-                transformOrigin: `center`
-              }}/>
-          </svg>
-
-          <div className="timer__value" xmlns="http://www.w3.org/1999/xhtml">
-            <span className="timer__mins">05</span>
-            <span className="timer__dots">:</span>
-            <span className="timer__secs">00</span>
-          </div>
-
-          <div className="game__mistakes">
-            <div className="wrong"></div>
-            <div className="wrong"></div>
-            <div className="wrong"></div>
-          </div>
-        </header>
+        <Header/>
 
         {this._getScreen()}
       </section>
